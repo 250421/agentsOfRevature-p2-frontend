584e73f323c6dce5ba89ee921f340d45b7eb3e82
Daniel Pham <danielkhp+work@gmail.com>
2025-05-13 15:34:28 -0500

    Readd needed dependency (class-variance-authority)

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -5,10 +5,10 @@ import { useConfirm } from "@/hooks/use-confirm";
 import { useSignOut } from "@/features/auth/hooks/use-sign-out";
 
 const ConfirmLogoutDialogProps = {
-  title: 'Are you sure you want to log out?',
-  description: 'You may lose your progress if you started a game.',
-  confirmLabel: 'Log Out',
-}
+  title: "Are you sure you want to log out?",
+  description: "You may lose your progress if you started a game.",
+  confirmLabel: "Log Out",
+};
 
 export function NavBar() {
   const [confirmLogout, ConfirmLogoutDialog] = useConfirm();
@@ -20,24 +20,28 @@ export function NavBar() {
     if (confirmed) {
       logout.mutate();
     }
-  }
+  };
 
   return (
     <header className="p-3 border-b">
       <div className="flex justify-between items-center">
         <div className="text-3xl font-bold w-[500px]">AGENTS OF REVATURE</div>
+
         <nav className="flex gap-5">
           {navItems.map((item: NavItem) => (
             <NavLink key={item.href} href={item.href} label={item.label} />
           ))}
         </nav>
+
         <div className="flex justify-end items-center w-[500px]">
           <nav>
             <NavLink href={"/profile"} label={"Agent {username}"} />
           </nav>
           <span className="ml-4">|</span>
-          <Button variant='ghost' onClick={handleLogoutClick}>Logout</Button>
-          <ConfirmLogoutDialog {...ConfirmLogoutDialogProps}/>
+          <Button variant="ghost" onClick={handleLogoutClick}>
+            Logout
+          </Button>
+          <ConfirmLogoutDialog {...ConfirmLogoutDialogProps} />
         </div>
       </div>
     </header>
